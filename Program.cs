b597263deea840b867872bef1c4bba6a80d2bdef
Taticsrm <tatianacoutinhoeng@gmail.com>
2023-01-30 05:12:28 -0300

    desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia (numero: "22335588", modelo: "NK684", imei: "1928376455", memoria: 4);
+nokia.Ligar ();
+nokia.InstalarAplicativo ("DIO");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone (numero: "987654321", modelo: "IP456", imei: "784512963", memoria: 120);
+iphone.Ligar  ();
+iphone.InstalarAplicativo ("Facebook");
+
+
